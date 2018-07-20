413712723a4797bcd10345ad0e80d5e4e1efd294
xinzizi77 <36582595+xinzizi77@users.noreply.github.com>
2018-07-20 10:13:54 +0800

    欢迎页

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,66 +1,67 @@
 // pages/welcome/welcome.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onLoad: function () {
+    // wx.login({
+    //   success: function (res) {
+    //     // console.log(res.code)
+    //     if (res.code) {
+    //       //发起网络请求
+    //       wx.request({
+    //         url: 'https://settime.kermi.xyz/frame/web/?r=user/login',
+    //         data: {
+    //           code: res.code
+    //         },
+    //         header: {
+    //           "content-type": "application/x-www-form-urlencoded" // 默认值
+    //         },
+    //         method: "POST",
+    //         success: function (res) {
+    //           console.log(res.data)
+    //           wx.setStorageSync('id', res.data.id);
+    //         },
+    //         fail: function () {
+    //           console.log("发送失败");
+    //         }
+    //       })
+    //     } else {
+    //       console.log('登录失败！' + res.errMsg)
+    //     }
+    //   }
+    // });
+    // // 查看是否授权
+    // wx.getSetting({
+    //   success: function (res) {
+    //     console.log(res.authSetting['scope.userInfo'])
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+    //       wx.getUserInfo({
+    //         success: function (res) {
+    //           wx.switchTab({//关闭当前页，跳到不相干的页面，没有返回
+    //             url: '../index/index'
+    //           })
+    //           console.log(res.userInfo);
+    //           wx.setStorageSync('userName', res.userInfo.nickName);
+    //           wx.setStorageSync('userpicture', res.userInfo.avatarUrl);
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  bindGetUserInfo: function (e) {
+    console.log(e.detail.userInfo)
+    wx.setStorageSync('userName', e.detail.userInfo.nickName);
+    wx.setStorageSync('userpicture', e.detail.userInfo.avatarUrl);
+    wx.switchTab({//关闭当前页，跳到不相干的页面，没有返回
+      url: '../index/index'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  into_home: function () {
+    wx.switchTab({//关闭当前页，跳到不相干的页面，没有返回
+      url: '../index/index'
+    })
   }
 })
