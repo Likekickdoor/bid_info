df3ce958ce13ba395498f0a0202d0bf1e00f1c9c
xinzizi77 <36582595+xinzizi77@users.noreply.github.com>
2018-07-21 09:37:19 +0800

    对接1

--- pages/key/key.js
+++ pages/key/key.js
@@ -1,18 +1,53 @@
 // pages/welcome/welcome.js
+function login_fun(){
+  wx.login({
+    success: function (res) {
+      // console.log(res.code)
+      if (res.code) {
+        //发起网络请求
+        wx.request({
+          url: 'https://m.ctrltab.xyz/bid_info/onlogin',
+          data: {
+            code: res.code,
+            face: wx.getStorageSync('userpicture'),
+            u_place: this.data. key1,
+            u_ind_type: this.data.key2,
+            timelong: this.data.key3
+          },
+          header: {
+            "content-type": "application/x-www-form-urlencoded" // 默认值
+          },
+          method: "POST",
+          success: function (res) {
+            console.log(res.data)
+            wx.setStorageSync('id', res.data.id);
+          },
+          fail: function () {
+            console.log("发送失败");
+          }
+        })
+      } else {
+        console.log('登录失败！' + res.errMsg)
+      }
+    }
+  });
+}
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    key1:"",
+    key2:"",
+    key3:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
@@ -62,5 +97,32 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  key1: function (e) {
+    this.setData({
+      key1: e.detail.value
+    })
+  },
+  key2: function (e) {
+    this.setData({
+      key2: e.detail.value
+    })
+  },
+  key3: function (e) {
+    this.setData({
+      key3: e.detail.value
+    })
+  },
+  finish:function(){
+    login_fun();
+    wx.redirectTo({
+      url: '../index/index',
+    })
+  },
+  skip: function () {
+    login_fun();
+    wx.redirectTo({
+      url: '../index/index',
+    })
   }
 })
