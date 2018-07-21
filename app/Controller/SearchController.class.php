c18fafe00b5a4b49c86015629b57cfa952480e79
Likekickdoor <day8th@163.com>
2018-07-21 15:41:13 +0800

    删除UserCtroller无用的函数

--- app/Controller/SearchController.class.php
+++ app/Controller/SearchController.class.php
@@ -1,3 +1,34 @@
 <?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use Interop\Container\ContainerInterface;
 
+
+class SearchController {
+	
+   protected $pdo;
+   public function __construct(ContainerInterface $container){
+   		$this->pdo=$container['db'];
+   }
+
+   /**
+	*@param 用户登录
+	*@return 返回数组
+   	*/
+   public function searchbid (Request $request, Response $response){
+		try{
+			
+		}catch(Exception $e){
+			$response = $response->withStatus($e->getCode())->withHeader('Content-type', 'application/json');
+			$response->getBody()->write(json_encode(
+				[
+		            'errorCode' => $e->getCode(),
+		            'error' => $e->getMessage()
+		        ]
+			));
+			return $response;
+		}
+   }
+
+}
 ?>
